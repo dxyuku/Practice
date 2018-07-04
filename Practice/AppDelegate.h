45df4f4222ccc305ddb9ff688c71aa71acb45b4e
duxinyuan <dxykevin@126.com>
2018-07-04 17:47:35 +0800

    Initial Commit

---
+++ Practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Practice
+//
+//  Created by dxykevin on 2018/7/4.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practice
+//
+//  Created by dxykevin on 2018/7/4.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
