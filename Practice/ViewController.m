855111bf16021dac45657a9bc3a3533941f7420a
duxinyuan <dxykevin@126.com>
2018-07-11 20:20:49 +0800

    冒泡排序

--- Practice/ViewController.m
+++ Practice/ViewController.m
@@ -25,6 +25,22 @@
     
     DXYLog(@"没办法，我就是这么强大。");
     NSLog(@"没办法，我就是这么强大。");
+    
+    int a[] = {2,6,3,85,23,12};
+    for (int i = 0; i < 6 - 1; i ++) {
+        for (int j = 0; j < 6 - 1 - i; j ++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+            
+        }
+    }
+    
+    for (int i = 0; i < 6; i ++) {
+        NSLog(@"%d ",a[i]);
+    }
 }
 
 - (void)testH {
